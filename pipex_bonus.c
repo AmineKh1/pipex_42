5b04acbe8854d6ff116745e0ab3594e9b90b8841
Amine Khouya <akhouya@e2r12p8.1337.ma>
2022-03-17 20:05:23 +0100

    bonus part

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -1,63 +1,86 @@
 
 #include "pipex.h"
 
+// char *command_exec(char **command, char **env){
+//     char *s;
+//     command = ft_split(argv[i], ' ');
+//     s = check_path(env, command[0]);
+//     return s;
+// }
 int main(int    argc, char*    argv[], char   **env) {
-    if(argc < 4)
-        return 0;
+    int fd[2];
+    pid_t pid;
+    int fd_in;
+    fd_in = 0;
     int i;
-    i = argc - 4;
-    int fd[i][2];
-
+    i = 2;
     char *s;
     char **command;
-    int j = i;
-    while(j > 0) {
-        if (pipe(fd[j]) < 0)
-            perror("error");
-        j--;
-    }
-    int id = fork();
-    if(id == 0)
+    int tab[1024];
+    if(argc < 5)
+        return 0;
+    while(i <= argc - 2)
     {
-        command = ft_split(argv[2], ' ');
+        command = ft_split(argv[i], ' ');
         s = check_path(env, command[0]);
-        int file1 = open(argv[1], O_RDWR);
-        dup2(file1, 0);
-        dup2(fd[i][1], 1);
-        execve(s, command, env);
-        exit(0);
-    }
-    waitpid(id, 0, 0);
-    j = 3;
-    while(i > 0)
-    {
-        id = fork();
-        if(id == 0)
+        printf("%s\n", s);
+        pipe(fd);
+        if((pid = fork()) == -1)
+            exit(1);
+        if(pid == 0)
         {
-            command = ft_split(argv[j], ' ');
-            s = check_path(env, command[0]);
-            dup2(fd[i][0], 0);
-            dup2(fd[i - 1][1], 1);
-            close(fd[i][1]);
-            close(fd[i - 1][0]);
-            execve(s, command, env);
+            if(i == 2)
+            {
+                int file1 = open(argv[1], O_RDWR);
+                if(s == NULL)
+                {
+                    printf("pipex: %s: command not found\n", argv[i]);
+                    exit(0);
+                }
+                dup2(file1, 0);
+                dup2(fd[1], 1);
+                close(fd[0]);
+                execve(s, command, env);
+            }
+            else if(i == argc - 2)
+            {
+                int file2 = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+                if(s == NULL)
+                {
+                    printf("pipex: %s: command not found\n", argv[i]);
+                    exit(0);
+                }
+                dup2(fd_in, 0);
+                dup2(file2, 1);
+                close(fd[1]);
+                close(fd[0]);
+                execve(s, command, env);
+            }
+            else
+            {
+                if(s == NULL)
+                {
+                    printf("pipex: %s: command not found\n", argv[i]);
+                    exit(0);
+                }
+                dup2(fd_in, 0);
+                dup2(fd[1], 1);
+                close(fd[0]);
+                execve(s, command, env);
+            }
         }
-        i--;
-        j++;
-        waitpid(id, 0, 0);
-    }
-    waitpid(id, 0, 0);
-    int id2 = fork();
-    if (id2 == 0)
-    {
-        command = ft_split(argv[j], ' ');
-        s = check_path(env, command[0]);
-        int file2 = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
-        dup2(file2, 1);
-        dup2(fd[0][0], 0);
-        close(fd[0][1]);
-        execve(s, command, env);
-    }
-    waitpid(id2, 0, 0);
+        else{
+            close(fd[1]);
+            fd_in = fd[0];
+            i++;
+            tab[i - 3] = fd[0];
+            
+        }
 
+    }
+    while(wait(NULL) != -1)
+    i = i - 3;
+    while(i >= 0)
+        close(tab[i--]);
+    
 }
